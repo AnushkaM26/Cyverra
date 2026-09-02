c247f4a3c59319251838e9366869152027343785
Anushka Magar <manushka138@gmail.com>
2026-09-02 23:12:25 +0530

    Time: 30 ms (61.99%) | Memory: 13.5 MB (66.04%) - LeetSync

---
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(robot.begin(), robot.end());
+        sort(factory.begin(), factory.end());
+
+        int n = robot.size();
+        int m = factory.size();
+
+        const long long INF = 1e18;
+
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, INF));
+
+        dp[0][0] = 0;
+
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = 0;
+
+            int position = factory[j - 1][0];
+            int limit = factory[j - 1][1];
+
+            for (int i = 1; i <= n; i++) {
+
+                dp[i][j] = dp[i][j - 1];
+
+                long long distance = 0;
+
+                for (int k = 1; k <= limit && k <= i; k++) {
+                    distance += abs((long long)robot[i - k] - position);
+
+                    if (dp[i - k][j - 1] != INF) {
+                        dp[i][j] = min(
+                            dp[i][j],
+                            dp[i - k][j - 1] + distance
+                        );
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
